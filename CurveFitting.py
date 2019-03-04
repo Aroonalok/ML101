730654cd5553c0d7c93cce577acffd84ac112427
Aroonalok Pyne <aroonalok.pyne@gmail.com>
2019-03-04 20:08:46 +0530

    Zooming out the view to see overfitting

--- CurveFitting.py
+++ CurveFitting.py
@@ -14,7 +14,7 @@ def get_function():
 def fit_curve(x,y,deg=1):
     z,residuals,_,_,_ = np.polyfit(x, y, deg, full=True)
     p = np.poly1d(z)
-    return p(x), residuals
+    return p, residuals
 
 
 if __name__ == "__main__":
@@ -22,8 +22,11 @@ if __name__ == "__main__":
 
     fit_results = [fit_curve(x,fx,d) for d in range(30)]
 
-    Y = np.c_[[poly for poly,err in fit_results]]
+    interval = np.ptp(x)
+
+    X = np.arange(x[0]-interval,x[-1]+interval)
+    Y = np.c_[[poly(X) for poly,err in fit_results]]
     err = np.c_[[err for poly,err in fit_results]]
 
-    viewer = Animator(X_MIN=0, X_MAX=100, Y_MIN=-1.5, Y_MAX=1.5)
-    viewer.visualize_data(x,fx,Y,err)
+    viewer = Animator(X_MIN=min(min(X),min(x)), X_MAX=max(max(X),max(x)), Y_MIN=1.5*min(fx), Y_MAX=1.5*max(fx))
+    viewer.visualize_data(x,fx,X,Y,err)
--- Visualize.py
+++ Visualize.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import numpy as np
+
 
 class Animator:
     def __init__(self,X_MIN,X_MAX,Y_MIN,Y_MAX):
@@ -9,13 +9,11 @@ class Animator:
         self.Y_MIN = Y_MIN
         self.Y_MAX = Y_MAX
         self.fig = plt.figure()
-        self.ax = plt.axes(xlim=(X_MIN, X_MAX), ylim=(Y_MIN, Y_MAX), title="Polynomial Curve Fitting", xlabel="x", ylabel="y")
-        # self.ax.grid()
+        self.ax = plt.axes(xlim=(X_MIN, X_MAX), ylim=(Y_MIN, Y_MAX), title="Curve Fitting", xlabel="x", ylabel="y")
         self.line, = self.ax.plot([], [], lw=1)
-        self.degree_text = self.ax.text(0.25, 0.95, '', transform=self.ax.transAxes)
-        self.error_text = self.ax.text(0.25, 0.90, '', transform=self.ax.transAxes)
-        self.x, self.y, self.err = None, None, None
-
+        self.degree_text = self.ax.text(0.5, 0.95, '', horizontalalignment='center', transform=self.ax.transAxes)
+        self.error_text = self.ax.text(0.5, 0.90, '', horizontalalignment='center', transform=self.ax.transAxes)
+        self.X, self.Y, self.err = None, None, None
 
     def init_frame(self):
         self.line.set_data([], [])
@@ -24,19 +22,18 @@ class Animator:
         return self.line, self.degree_text, self.error_text
 
     def animate(self,i):
-        self.line.set_data(self.x, self.y[i])
+        self.line.set_data(self.X, self.Y[i])
         self.degree_text.set_text('Degree = {}'.format(i))
         self.error_text.set_text('Error = {}'.format(self.err[i][0]))
         return self.line, self.degree_text, self.error_text
 
-    def visualize_data(self,x_in,fx_in,y_in,error):
-        self.x = x_in
-        self.y = y_in
+    def visualize_data(self,x_in,fx_in,x_out,y_out,error):
+        self.X = x_out
+        self.Y = y_out
         self.err = error
         self.ax.plot(x_in,fx_in,'-o',lw=1)
-        degrees,_ = y_in.shape
+        degrees,_ = y_out.shape
         anim = animation.FuncAnimation(self.fig, self.animate, init_func=self.init_frame,
                                        frames=degrees, interval=1000, blit=True)
         anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-
         plt.show()
